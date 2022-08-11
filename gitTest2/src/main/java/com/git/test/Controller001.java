ce0ab2019372b6e9f43aa89ea6e22eac7cd9c91b
NB-21122703\NB-000000 <donghwan.kim@bankwareglobal.com>
2022-08-11 23:41:43 +0900

    컨트롤러 커밋

---
+++ gitTest2/src/main/java/com/git/test/Controller001.java
@@ -0,0 +1,19 @@
+package com.git.test;
+
+import bxm.common.annotaion.BxmCategory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller001
+ *
+ * @author sysadmin
+ */
+@RestController
+@RequestMapping("/")
+@BxmCategory(logicalName="Controller001", description="Controller001")
+public class Controller001 {
+	private Logger logger= LoggerFactory.getLogger(getClass());
+}
