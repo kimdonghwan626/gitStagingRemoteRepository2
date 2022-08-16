bc224743f223d500f55c93fad7cb2c2f41756f93
NB-21122703\NB-000000 <donghwan.kim@bankwareglobal.com>
2022-08-16 17:54:47 +0900

    커밋

--- gitTest2/src/main/java/com/git/test/Controller001.java
+++ gitTest2/src/main/java/com/git/test/Controller001.java
@@ -16,8 +16,5 @@ import org.springframework.web.bind.annotation.RestController;
 @BxmCategory(logicalName="Controller001", description="Controller001")
 public class Controller001 {
 	private Logger logger= LoggerFactory.getLogger(getClass());
-	
-	public void test() {
-		System.out.println()
-	}
+
 }
---
+++ gitTest2/src/main/java/com/git/test/Dbio001.java
@@ -0,0 +1,24 @@
+/**
+ * Generated 2022. 08. 16. 17:48:17
+ */
+package com.git.test;
+
+import bxm.common.annotaion.BxmCategory;
+import bxmc.annotation.dbio.BxmDataAccess;
+
+/**
+ * Dbio001
+ *
+ * @author sysadmin
+ */
+@SuppressWarnings({ "all" })
+@BxmDataAccess(datasource = "main-ds")
+@BxmCategory(logicalName="Dbio001", description="Dbio001", author="sysadmin")
+public interface Dbio001
+{
+	/**
+	 * 
+	 */
+	@BxmCategory(logicalName="", description="", author="sysadmin")
+	String selectId();
+}
